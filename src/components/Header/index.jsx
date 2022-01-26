e7a91c88b1eb3802590c4c63a97b314e39229e91
Rafael Tivane <luislangabusiness@gmail.com>
2022-01-26 15:33:25 +0200

    adding drawer and customizing its layout

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,44 @@
-import { IconButton, Typography } from '@mui/material';
+import { Chip, Drawer, IconButton, Paper, Typography } from '@mui/material';
 import classNames from 'classnames'
 import { useDisplay, useGlobalStyles, useTypography } from '../../styles'
 import MenuIcon from '@mui/icons-material/Menu';
 import { useStyles } from './styles'
+import { useCallback, useMemo, useState } from 'react';
+import CloseIcon from '@mui/icons-material/Close';
 
 const Header = () => {
     const classes = useStyles();
     const display = useDisplay();
     const globalStyles = useGlobalStyles();
     const text = useTypography();
+
+    const [ open, setOpen ] = useState(false);
+    const closeMenu = useCallback(() => setOpen(false), []);
+    const toggleMenu = useCallback(() => setOpen(o => !o), []);
+
+    const CustomChip = useCallback(({ label }) => (
+        <Chip
+            label={ label }
+            onClick={() => {}}
+            className={classNames(display.mr1, display.mb1)} 
+        />
+    ), [ display ])
+
+    const drawerContent = useMemo(() => (
+        <>
+            <Paper 
+                className={classNames(display.pt1, display.pb2, globalStyles.px, display.flex, display.alignCenter,
+                display.wrap, classes.paper)}
+                elevation={0}>
+                <CustomChip label="All" />
+                <CustomChip label="UI" />
+                <CustomChip label="UX" />
+                <CustomChip label="Ehancement" />
+                <CustomChip label="Bug" />
+                <CustomChip label="Feature" />
+            </Paper>
+        </>
+    ), [ classes, display, globalStyles ])
 
     return (
         <header>
@@ -19,10 +49,21 @@ const Header = () => {
                     <Typography component="h1" variant="h6">Frontend Mentor</Typography>
                     <Typography className={classNames(text.rem9, display.opacity9)}>Feedback Board</Typography>
                 </div>
-                <IconButton>
-                    <MenuIcon className={classNames(text.textLight)} />
+                <IconButton
+                    onClick={toggleMenu}>
+                    { open ? <CloseIcon className={classNames(text.textLight)} /> : <MenuIcon className={classNames(text.textLight)} /> }
                 </IconButton>
             </div>
+            <Drawer
+                anchor="right"
+                open={open}
+                onClose={closeMenu}
+                classes={{ paper: classes.drawerPaper, root: classes.drawerRoot }}
+                >
+                <div className={classNames(globalStyles.px, classes.drawerContent, display.pt2, display.pb2)}>
+                    { drawerContent }
+                </div>
+            </Drawer>
         </header>
     );
 };
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -3,5 +3,21 @@ import { makeStyles } from '@mui/styles';
 export const useStyles = makeStyles(theme => ({ 
     headerHighlight: {
         backgroundImage: 'linear-gradient(205deg, #E84D70, #A337F6, #28A7ED)',
+    },
+    drawerRoot: {
+        top: '86px !important',
+        '& .MuiBackdrop-root': {
+            top: '86px !important',
+        }
+    },
+    drawerPaper: {
+        top: '86px !important',
+        width: '80%'
+    },
+    drawerContent: {
+        backgroundColor: '#F2F4FF'
+    },
+    paper: {
+        borderRadius: '16px !important'
     }
 }));
