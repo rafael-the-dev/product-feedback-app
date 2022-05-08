449fab8f6ac1a58c9255e1a9e9ff516ecceffb49
Rafael Tivane <luislangabusiness@gmail.com>
2022-05-08 18:25:47 +0200

    create login page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3f92fa2e85e8604a"; }
+/******/ 		__webpack_require__.h = function() { return "bdd4c48ac435c9f9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ src/pages/login/index.jsx
@@ -0,0 +1,95 @@
+import { Button, FormControl, InputAdornment, InputLabel, IconButton, OutlinedInput, Paper, TextField } from '@mui/material';
+import { useCallback, useRef, useState } from 'react'
+import Link from 'next/link'
+
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import Visibility from '@mui/icons-material/Visibility';
+
+import classNames from 'classnames'
+
+const Container = () => {
+    const userNameRef = useRef(null);
+    const [ values, setValues ] = useState({
+        password: '',
+        showPassword: false,
+    });
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
+
+    const handleClickShowPassword = useCallback(() => {
+        setValues(currentValues => ({
+          ...currentValues,
+          showPassword: !currentValues.showPassword
+        }));
+    }, []);
+
+    const handleChange = useCallback((prop) => (event) => {
+        setValues(currentValues => ({ ...currentValues, [prop]: event.target.value }));
+    }, []);
+
+    return (
+        <Paper 
+            component="form"
+            elavation={0}>
+            <fieldset>
+                <TextField
+                    id="username-textfield"
+                    label="TextField"
+                    fullWidth
+                    className={classNames("mt-4")}
+                    inputRef={userNameRef}
+                    InputProps={{
+                    startAdornment: (
+                        <InputAdornment position="start">
+                            <AccountCircle />
+                        </InputAdornment>
+                    ),
+                    }}
+                    variant="outlined"
+                />
+                <FormControl fullWidth variant="outlined" className={classNames("mt-4")}>
+                    <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                    <OutlinedInput
+                        id="outlined-adornment-password"
+                        type={values.showPassword ? 'text' : 'password'}
+                        value={values.password}
+                        onChange={handleChange('password')}
+                        endAdornment={
+                        <InputAdornment position="end">
+                            <IconButton
+                                aria-label="toggle password visibility"
+                                onClick={handleClickShowPassword}
+                                onMouseDown={handleMouseDownPassword}
+                                edge="end"
+                            >
+                                {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                            </IconButton>
+                        </InputAdornment>
+                        }
+                        label="Password"
+                    />
+                    </FormControl>
+                    <div 
+                        className={classNames("flex items-center mt-4")}
+                        >
+                        <Button 
+                            variant="contained"
+                            type="submit"
+                        >Submit
+                        </Button>
+                        <p>
+                            don't you have an account? 
+                            <Link href="/signup" className={classNames("ml-4")}>
+                                <a>sign up.</a>
+                            </Link>
+                        </p>
+                    </div>
+            </fieldset>
+        </Paper>
+    );
+};
+
+export default Container;
